7996347476fef4faa2af979a2a61a4f099d31d0b
Szewoj <wojciech_szerszen@op.pl>
2024-06-09 17:25:04 +0200

    [add] script for steering through wifi

---
+++ RadioSteering-PCside/wifiSteer.py
@@ -0,0 +1,124 @@
+from inputs import get_gamepad
+import math
+import threading
+import sys
+
+class XboxController:
+    # XboxController class by Brian Zier and Kevin Hughes. (2017 MIT-license)
+    # https://github.com/bzier/TensorKart/blob/master/utils.py
+    MAX_TRIG_VAL = math.pow(2, 8)
+    MAX_JOY_VAL = math.pow(2, 15)
+
+    def __init__(self):
+
+        self.LeftJoystickY = 0
+        self.LeftJoystickX = 0
+        self.RightJoystickY = 0
+        self.RightJoystickX = 0
+        self.LeftTrigger = 0
+        self.RightTrigger = 0
+        self.LeftBumper = 0
+        self.RightBumper = 0
+        self.A = 0
+        self.X = 0
+        self.Y = 0
+        self.B = 0
+        self.LeftThumb = 0
+        self.RightThumb = 0
+        self.Back = 0
+        self.Start = 0
+        self.LeftDPad = 0
+        self.RightDPad = 0
+        self.UpDPad = 0
+        self.DownDPad = 0
+
+        self._monitor_thread = threading.Thread(target=self._monitor_controller, args=())
+        self._monitor_thread.daemon = True
+        self._monitor_thread.start()
+
+
+    def read(self):
+        xL = self.LeftJoystickX
+        yL = self.LeftJoystickY
+        xR = self.RightJoystickX
+        yR = self.RightJoystickY
+        trig = self.RightTrigger - self.LeftTrigger
+
+        # translate to mobile base control signals:
+        throttle = round(trig*100)
+        steering = round(xL*100)
+
+        updown = round(yL * 100 / 2)
+        xAxis = round(xR * 100 / 2)
+        yAxis = round(yR * 100 / 2)
+
+        uFL = min(100, max(0, 50 + updown + xAxis - yAxis))
+        uFR = min(100, max(0, 50 + updown - xAxis - yAxis))
+        uBL = min(100, max(0, 50 + updown + xAxis + yAxis))
+        uBR = min(100, max(0, 50 + updown - xAxis + yAxis))
+
+        return [throttle, steering, uFL, uFR, uBL, uBR]
+
+
+    def _monitor_controller(self):
+        while True:
+            events = get_gamepad()
+            for event in events:
+                if event.code == 'ABS_Y':
+                    self.LeftJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_X':
+                    self.LeftJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RY':
+                    self.RightJoystickY = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_RX':
+                    self.RightJoystickX = event.state / XboxController.MAX_JOY_VAL # normalize between -1 and 1
+                elif event.code == 'ABS_Z':
+                    self.LeftTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'ABS_RZ':
+                    self.RightTrigger = event.state / XboxController.MAX_TRIG_VAL # normalize between 0 and 1
+                elif event.code == 'BTN_TL':
+                    self.LeftBumper = event.state
+                elif event.code == 'BTN_TR':
+                    self.RightBumper = event.state
+                elif event.code == 'BTN_SOUTH':
+                    self.A = event.state
+                elif event.code == 'BTN_NORTH':
+                    self.Y = event.state #previously switched with X
+                elif event.code == 'BTN_WEST':
+                    self.X = event.state #previously switched with Y
+                elif event.code == 'BTN_EAST':
+                    self.B = event.state
+                elif event.code == 'BTN_THUMBL':
+                    self.LeftThumb = event.state
+                elif event.code == 'BTN_THUMBR':
+                    self.RightThumb = event.state
+                elif event.code == 'BTN_SELECT':
+                    self.Back = event.state
+                elif event.code == 'BTN_START':
+                    self.Start = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY1':
+                    self.LeftDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY2':
+                    self.RightDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY3':
+                    self.UpDPad = event.state
+                elif event.code == 'BTN_TRIGGER_HAPPY4':
+                    self.DownDPad = event.state
+
+
+
+def main():
+    joy = XboxController()
+    try:
+        while True:
+            print(joy.read())
+    except KeyboardInterrupt:
+        print('Program closing...')
+        # do stuff
+        sys.exit(0)
+
+
+
+if __name__ == '__main__':
+    main()
+    
